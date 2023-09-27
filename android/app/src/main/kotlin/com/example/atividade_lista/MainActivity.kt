691caed4f8b70d13e4443b1eea965f9872fa602c
Eze <ezeseihe@gmail.com>
2023-09-27 17:33:30 -0300

    Atividade Lista de Jogos

---
+++ android/app/src/main/kotlin/com/example/atividade_lista/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.atividade_lista
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class Game {
+  final String name;
+  final String genre;
+  final String imagePath;
+
+  Game({
+    required this.name,
+    required this.genre,
+    required this.imagePath,
+  });
+}
+
+class WelcomeScreen extends StatelessWidget {
+  final VoidCallback onButtonPressed;
+
+  WelcomeScreen({required this.onButtonPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Bem-vindo ao App de Jogos'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Bem-vindo!',
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: onButtonPressed,
+              child: Text('Ver Lista de Jogos'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  final List<Game> games = [
+    Game(name: 'The Witcher 3', genre: 'RPG', imagePath: 'assets/witcher3.png'),
+    Game(
+        name: 'Grand Theft Auto V',
+        genre: 'Action',
+        imagePath: 'assets/gtaV.png'),
+    Game(
+        name: 'The Legend of Zelda',
+        genre: 'Adventure',
+        imagePath: 'assets/zelda.png'),
+    // Adicione mais jogos conforme necessário
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => WelcomeScreen(
+              onButtonPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => GameListScreen(games: games),
+                  ),
+                );
+              },
+            ),
+      },
+    );
+  }
+}
+
+class GameListScreen extends StatelessWidget {
+  final List<Game> games;
+
+  GameListScreen({required this.games});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Lista de Jogos'),
+      ),
+      body: ListView.builder(
+        itemCount: games.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: Image.asset(games[index].imagePath),
+            title: Text(games[index].name),
+            subtitle: Text(games[index].genre),
+            onTap: () {
+              // Você pode adicionar ação personalizada quando um item da lista é clicado aqui
+            },
+          );
+        },
+      ),
+    );
+  }
+}
